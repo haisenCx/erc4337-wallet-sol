9f5100f78b78fca21ae09b8b7cd098e211b73334
Myron <Myron.zhang@coinstore.com>
2023-07-16 12:35:58 +0800

    fix: deploy

--- script/deploy-erc4337.js
+++ script/deploy-erc4337.js
@@ -34,6 +34,7 @@ async function main() {
 
     console.log("Deploy contract EOA address: " + addr.address);
 
+    console.log("[EntryPoint] start to deploy");
     const entryPointFactory = await ethers.getContractFactory("EntryPoint");
     const entryPoint = await entryPointFactory.deploy()
     await entryPoint.deployed();
@@ -41,6 +42,7 @@ async function main() {
     console.log("[EntryPoint] ConstructorArguments: " + []);
     await verifyOnBlockscan(entryPoint.address, [], null)
 
+    console.log("[SimpleAccountFactory] start to deploy");
     const simpleAccountFFactory = await ethers.getContractFactory("SimpleAccountFactory");
     const simpleAccountF = await simpleAccountFFactory.deploy(entryPoint.address);
     await simpleAccountF.deployed();
@@ -48,16 +50,7 @@ async function main() {
     console.log("[SimpleAccountFactory] ConstructorArguments: " + [entryPoint.address]);
     await verifyOnBlockscan(simpleAccountF.address, [entryPoint.address], "contracts/erc4337/samples/SimpleAccountFactory.sol:SimpleAccountFactory")
 
-    // const simpleAccountFactory = await ethers.getContractFactory("SimpleAccount");
-    // const simpleAccount = await simpleAccountFactory.deploy(entryPoint.address);
-    // await simpleAccount.deployed();
-    // console.log("[SimpleAccount] contract address: " + simpleAccount.address);
-    // console.log("[SimpleAccount] ConstructorArguments: " + [entryPoint.address]);
-    // await verifyOnBlockscan(simpleAccount.address, [entryPoint.address], null)
-
-    // set wallet account owner
-    // await simpleAccount.initialize(addr.address);
-
+    console.log("[TokenPaymaster] start to deploy");
     const tokenPaymasterFactory = await ethers.getContractFactory("SWTokenPaymaster");
     const tokenPaymaster = await tokenPaymasterFactory.deploy(simpleAccountF.address, "SWT", entryPoint.address);
     await tokenPaymaster.deployed();
@@ -65,6 +58,7 @@ async function main() {
     console.log("[TokenPaymaster] ConstructorArguments: " + [simpleAccountF.address, "SWT", entryPoint.address]);
     await verifyOnBlockscan(tokenPaymaster.address, [simpleAccountF.address, "SWT", entryPoint.address], null)
 
+    console.log("[Success] All contracts have been deployed success.")
 }
 
 async function verifyOnBlockscan(address, args, contractPath) {
@@ -79,7 +73,7 @@ async function verifyOnBlockscan(address, args, contractPath) {
                 params["contract"] = contractPath;
             }
             await hre.run("verify:verify", params);
-            console.log("Script executed successfully");
+            console.log("verify successfully");
             success = true;
         } catch (error) {
             console.log(`Script failed: ${error}`);
